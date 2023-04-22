304c6597ea57790b9eef9213873a03de913b790c
Kallo Gyorgy David <kallogyorgy5@gmail.com>
2023-04-22 20:36:30 +0200

    Added menu scene.

--- src/main/java/game/gui/GameApplication.java
+++ src/main/java/game/gui/GameApplication.java
@@ -12,7 +12,7 @@ public class GameApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/ui.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/menu.fxml"));
         stage.setTitle("Labyrinth Game");
         Scene scene = new Scene(root);
         stage.setScene(scene);
---
+++ src/main/java/game/gui/MenuController.java
@@ -0,0 +1,32 @@
+package game.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuController {
+    @FXML
+    private Button startButton;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent parent;
+
+    public void startGame(ActionEvent actionEvent) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("/ui.fxml"));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+
+    }
+}
