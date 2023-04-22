190756adaa3c010dfcf8be0eac5460bd31bffe6c
Kallo Gyorgy David <kallogyorgy5@gmail.com>
2023-04-22 17:01:11 +0200

    Using sliding-puzzle-game as template.

---
+++ src/main/java/puzzle/gui/GameApplication.java
@@ -0,0 +1,22 @@
+package puzzle.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GameApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/ui.fxml"));
+        stage.setTitle("Sliding Puzzle Game");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
